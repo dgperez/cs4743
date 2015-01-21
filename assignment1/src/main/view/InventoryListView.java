e02d5ce5a8a9603fff0c8fd04de41d3e2230741e
Kyle Haley <kylephaley@gmail.com>
2015-01-21 17:52:48 -0600

    Forgot to add InventoryListView class to repo

--- assignment1/src/main/view/InventoryListView.java
+++ assignment1/src/main/view/InventoryListView.java
@@ -1,6 +1,8 @@
 package main.view;
 
-public class InventoryListView {
+import javax.swing.JFrame;
+
+public class InventoryListView extends JFrame {
 
 	public InventoryListView() {
 		// TODO Auto-generated constructor stub
