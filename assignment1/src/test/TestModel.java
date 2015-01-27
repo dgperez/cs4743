344dfd4ae7b6ae03cb533e0c4dafa8135465a7ed
JoshSmithUTSA <josh.smith.sa@gmail.com>
2015-01-26 18:46:17 -0600

    Added test cases for Item

--- assignment1/src/test/TestModel.java
+++ assignment1/src/test/TestModel.java
@@ -1,9 +1,54 @@
 package test;
 
+import static org.junit.Assert.*;
+import main.model.Item;
+
+import org.junit.Test;
+
 public class TestModel {
 
-	public TestModel() {
-		// TODO Auto-generated constructor stub
+	@Test
+	public void createItemTest1() {
+		Item i = new Item("1", "partNameGoesHere", "", 10);
+		assertTrue(i.getPartName().equals("partNameGoesHere") && 
+		   i.getVendor().equals("") && 
+		   i.getQuantity() == 10);
+	}
+	
+	@Test
+	public void createItemTest2() {
+		Item i = new Item("1", "partNameGoesHere", "vendorNameGoesHere", 10);
+		assertTrue(i.getPartName().equals("partNameGoesHere") && 
+		   i.getVendor().equals("vendorNameGoesHere") && 
+		   i.getQuantity() == 10);
 	}
 
+	@Test
+	public void editpartNumberTest() {
+		Item i = new Item("1", "partNameGoesHere", "vendorNameGoesHere", 10);
+		i.setPartNumber("2");
+		assertTrue(i.getPartNumber().equals("2"));
+	}
+
+	@Test
+	public void editpartNameTest() {
+		Item i = new Item("1", "partNameGoesHere", "vendorNameGoesHere", 10);
+		i.setPartName("newPartName");
+		assertTrue(i.getPartName().equals("newPartName"));
+	}
+
+	@Test
+	public void editVendorTest() {
+		Item i = new Item("1", "partNameGoesHere", "vendorNameGoesHere", 10);
+		i.setVendor("newVendor");
+		assertTrue(i.getVendor().equals("newVendor"));
+	}
+
+	@Test
+	public void editQuantityTest() {
+		Item i = new Item("1", "partNameGoesHere", "vendorNameGoesHere", 10);
+		i.setQuantity(1);
+		assertTrue(i.getQuantity() == 1);
+	}
+	
 }
