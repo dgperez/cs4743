807593f2c73bddfda660b6a457b6cc4b58b9ed3a
Kyle Haley <kylephaley@gmail.com>
2015-03-16 17:36:51 -0500

    Filled in list views

--- assignment1/src/main/view/ProductTemplatePartsListView.java
+++ assignment1/src/main/view/ProductTemplatePartsListView.java
@@ -1,19 +1,82 @@
 package main.view;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import main.model.ProductTemplate;
 
 public class ProductTemplatePartsListView extends JFrame {
+	
+	private ProductTemplate productTemplate;
+	
+	private JList<Object> list;
+	
+	private JButton addProductTemplatePart;
+	
+	private JButton deleteProductTemplatePart;
+	
+	private JScrollPane scrollPane;
+	
+	private JPanel controls;
+	
+	private JPanel panel;
 
-	public ProductTemplatePartsListView() {
-		// TODO Auto-generated constructor stub
+	public ProductTemplatePartsListView(ProductTemplate productTemplate) {
+		super("Template Parts List");
+		
+		this.productTemplate = productTemplate;
+		
+		this.panel = new JPanel();
+		this.panel.setLayout(new BorderLayout());
+		this.panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		
+		this.list = new JList<Object>(
+				this.productTemplate.getProductTemplateParts().toArray());
+		
+		this.scrollPane = new JScrollPane();
+		this.scrollPane.getViewport().add(this.list);
+		this.scrollPane.setPreferredSize(new Dimension(250, 200));
+		this.panel.add(this.scrollPane);
+		
+		this.controls = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 2));
+		
+		this.addProductTemplatePart = new JButton("Add Template Part");
+		this.addProductTemplatePart.setActionCommand("addTemplatePart");
+		this.controls.add(this.addProductTemplatePart);
+		
+		this.deleteProductTemplatePart = new JButton("Delete Template Part");
+		this.deleteProductTemplatePart.setActionCommand("deleteTemplatePart");
+		this.controls.add(this.deleteProductTemplatePart);
+		
+		add(this.panel, BorderLayout.CENTER);
+		add(this.controls, BorderLayout.SOUTH);
+		
+		pack();
+		
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
+	
+	public void registerListener(){}
 
 	public void refreshList(ProductTemplate productTemplate){
-		throw new NotImplementedException();
+		this.productTemplate = productTemplate;
+		this.list.setListData(
+				this.productTemplate.getProductTemplateParts().toArray());
+		this.list.repaint();
+	}
+	
+	public Object getSelectedItem(){
+		return this.list.getSelectedValue();
 	}
 	
 }
--- assignment1/src/main/view/ProductTemplatesListView.java
+++ assignment1/src/main/view/ProductTemplatesListView.java
@@ -1,20 +1,83 @@
 package main.view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import main.model.ProductTemplates;
 
 public class ProductTemplatesListView extends JFrame {
+
+	private ProductTemplates productTemplates;
 	
-	private ProductTemplates templates;
+	private JList<Object> list;
+	
+	private JButton addProductTemplate;
+	
+	private JButton deleteProductTemplate;
+	
+	private JScrollPane scrollPane;
+	
+	private JPanel controls;
+	
+	private JPanel panel;
 
-	public ProductTemplatesListView() {
-		// TODO Auto-generated constructor stub
+	public ProductTemplatesListView(ProductTemplates productTemplates) {
+		super("Cabinetron Product Templates");
+		
+		this.productTemplates = productTemplates;
+		
+		this.panel = new JPanel();
+		this.panel.setLayout(new BorderLayout());
+		this.panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		
+		this.list = new JList<Object>(
+				this.productTemplates.getProductTemplates().toArray());
+		
+		this.scrollPane = new JScrollPane();
+		this.scrollPane.getViewport().add(this.list);
+		this.scrollPane.setPreferredSize(new Dimension(250, 200));
+		this.panel.add(this.scrollPane);
+		
+		this.controls = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 2));
+		
+		this.addProductTemplate = new JButton("Add Template");
+		this.addProductTemplate.setActionCommand("addproducttemplate");
+		this.controls.add(this.addProductTemplate);
+		
+		this.deleteProductTemplate = new JButton("Delete Template");
+		this.deleteProductTemplate.setActionCommand("deletetemplate");
+		this.controls.add(this.deleteProductTemplate);
+		
+		add(this.panel, BorderLayout.CENTER);
+		add(this.controls, BorderLayout.SOUTH);
+		
+		pack();
+		
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
+	
+	public void registerListener(){}
 
-	public void refreshList(ProductTemplates templates){
-		throw new NotImplementedException();
+	public void refreshList(ProductTemplates productTemplates){
+		this.productTemplates = productTemplates;
+		this.list.setListData(
+				this.productTemplates.getProductTemplates().toArray());
+		this.list.repaint();
+	}
+	
+	public Object getSelectedListItem(){
+		return this.list.getSelectedValue();
 	}
 }
