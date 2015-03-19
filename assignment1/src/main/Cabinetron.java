238ae1d98afeff510b2fdb1b464bfe056d0b6c56
Kyle Haley <kylephaley@gmail.com>
2015-03-18 19:52:08 -0500

    Fixed comments in Cabinetron

--- assignment1/src/main/Cabinetron.java
+++ assignment1/src/main/Cabinetron.java
@@ -17,7 +17,7 @@ import main.view.ProductTemplateListView;
 
 public class Cabinetron {
 
-	/** CS 4743 Assignment 1 by Josh Smith and Kyle Haley
+	/** CS 4743 Assignment 4 by Josh Smith and Kyle Haley
 	 * 
 	 * */
 	public static void main(String[] args){
